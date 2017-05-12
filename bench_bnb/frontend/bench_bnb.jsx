edf8f55d7fd5ff8c74278207a090a99927e83916
Tu Ngo <tungo8810@gmail.com>
2017-05-12 15:36:42 -0700

    Add Greeting Components

--- bench_bnb/frontend/bench_bnb.jsx
+++ bench_bnb/frontend/bench_bnb.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+import * as SessionAction from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.store = store;
+  window.SessionAction = SessionAction;
+
   const root = document.getElementById("root");
   ReactDOM.render(
-    <h1>React is working!</h1>,
+    <Root store={store} />,
     root
   );
 });
