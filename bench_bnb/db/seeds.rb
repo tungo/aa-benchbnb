0051367f4c9233c737f5ce217fd8486713df2156
Tu Ngo <tungo8810@gmail.com>
2017-05-15 09:39:33 -0700

    Seed data

--- bench_bnb/db/seeds.rb
+++ bench_bnb/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+b1 = Bench.create(description: 'Bench 1', lat: 37.792246, lng: -122.403115)
+b2 = Bench.create(description: 'Bench 2', lat: 37.795849, lng: -122.397594)
+b3 = Bench.create(description: 'Bench 3', lat: 37.792071, lng: -122.392447)
+b4 = Bench.create(description: 'Bench 4', lat: 37.791494, lng: -122.389958)
+b5 = Bench.create(description: 'Bench 5', lat: 37.784914, lng: -122.402627)
