38e75d71f819a1ae4d44ccaf727315cabe39858a
Tu Ngo <tungo8810@gmail.com>
2017-05-15 10:20:51 -0700

    Add BenchesReducer to RootReducer

--- bench_bnb/frontend/reducers/root_reducer.js
+++ bench_bnb/frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import BenchesReducer from './benches_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  benches: BenchesReducer
 });
 
 export default RootReducer;
