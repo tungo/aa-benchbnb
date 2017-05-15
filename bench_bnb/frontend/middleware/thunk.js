edd231f7f4119e24b826e136dac823b7780890b0
Tu Ngo <tungo8810@gmail.com>
2017-05-15 15:21:43 -0700

    Update to use redux-thunk

--- bench_bnb/frontend/middleware/thunk.js
+++
@@ -1,8 +0,0 @@
-const thunk = ({dispatch}) => next => action => {
-  if (typeof action === 'function') {
-    return action(dispatch);
-  }
-  return next(action);
-};
-
-export default thunk;
--- bench_bnb/frontend/store/store.js
+++ bench_bnb/frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from '../middleware/thunk';
+import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
