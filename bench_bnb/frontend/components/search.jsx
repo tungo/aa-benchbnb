360e29d7a3fa37cd21faa117eb26b4b98c759145
Tu Ngo <tungo8810@gmail.com>
2017-05-15 10:49:16 -0700

    Update Search component

--- bench_bnb/frontend/components/search.jsx
+++ bench_bnb/frontend/components/search.jsx
@@ -3,18 +3,12 @@ import React from 'react';
 import BenchMap from './bench_map';
 import BenchIndex from './bench_index';
 
-class Search extends React.Component {
-  render() {
-    return (
-      <div>
-        <BenchMap />
-        <BenchIndex
-          benches={this.props.benches}
-          fetchBenches={this.props.fetchBenches}
-        />
-      </div>
-    );
-  }
-}
-
-export default Search;
+export default ({ benches, fetchBenches }) => (
+  <div>
+    <BenchMap />
+    <BenchIndex
+      benches={benches}
+      fetchBenches={fetchBenches}
+    />
+  </div>
+);
