1cc9cb5f0cb0f5674fa5cb7cf6dc194d596778ee
Tu Ngo <tungo8810@gmail.com>
2017-05-15 11:16:09 -0700

    Implement Google map into BenchMap

--- bench_bnb/frontend/components/bench_map.jsx
+++ bench_bnb/frontend/components/bench_map.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class BenchMap extends React.Component {
+  componentDidMount() {
+    // SF
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 },
+      zoom: 13
+    };
+
+    // wrap the mapDOMNode in a Google Map
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
   render() {
     return (
-      <div id="map-container" ref="map">
+      <div id="map-container" ref={(map) => (this.mapNode = map)}>
 
       </div>
     );
