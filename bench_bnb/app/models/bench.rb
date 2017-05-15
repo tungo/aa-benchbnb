459e83b5a6613a809b9582161e307c02af6313b7
Tu Ngo <tungo8810@gmail.com>
2017-05-15 13:54:27 -0700

    Add get benches by bounds

--- bench_bnb/app/models/bench.rb
+++ bench_bnb/app/models/bench.rb
@@ -1,3 +1,10 @@
 class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
+
+  def self.in_bounds(bounds)
+    self.where("lat <= #{bounds['northWest']}")
+        .where("lat >= #{bounds['southEast']}")
+        .where("lng <= #{bounds['northWest']}")
+        .where("lng >= #{bounds['southEast']}")
+  end
 end
