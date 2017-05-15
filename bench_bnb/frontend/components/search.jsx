9c0bc9d513d6c76d132c25cb6fb564f2668ad082
Tu Ngo <tungo8810@gmail.com>
2017-05-15 11:23:40 -0700

    Add MarkerManager class

--- bench_bnb/frontend/components/search.jsx
+++ bench_bnb/frontend/components/search.jsx
@@ -5,7 +5,7 @@ import BenchIndex from './bench_index';
 
 export default ({ benches, fetchBenches }) => (
   <div>
-    <BenchMap />
+    <BenchMap benches={benches} />
     <BenchIndex
       benches={benches}
       fetchBenches={fetchBenches}
