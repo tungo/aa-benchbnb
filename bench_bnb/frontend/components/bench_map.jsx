46d60feede905b8e0fc00648eb4dd6933dcdc88a
Tu Ngo <tungo8810@gmail.com>
2017-05-15 10:40:11 -0700

    Add BenchMap component and #map-container css size

---
+++ bench_bnb/frontend/components/bench_map.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BenchMap extends React.Component {
+  render() {
+    return (
+      <div id="map-container" ref="map">
+
+      </div>
+    );
+  }
+}
+
+export default BenchMap;
