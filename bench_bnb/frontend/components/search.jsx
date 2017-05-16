7ae07029c99a5bfcabfd4595ff016a79331e9baf
Tu Ngo <tungo8810@gmail.com>
2017-05-15 17:43:22 -0700

    Implement FilterForm into Search component

--- bench_bnb/frontend/components/search.jsx
+++ bench_bnb/frontend/components/search.jsx
@@ -3,11 +3,24 @@ import React from 'react';
 import BenchMap from './bench_map';
 import BenchIndex from './bench_index';
 
-export default ({ benches, fetchBenches, changeFilter }) => (
+export default ({
+  benches,
+  minSeating,
+  maxSeating,
+  fetchBenches,
+  changeFilter,
+  updateFilter
+}) => (
   <div>
     <BenchMap
       benches={benches}
       changeFilter={changeFilter}
+      updateFilter={updateFilter}
+    />
+    <FilterForm
+      minSeating={minSeating}
+      maxSeating={maxSeating}
+      updateFilter={updateFilter}
     />
     <BenchIndex
       benches={benches}
--- bench_bnb/frontend/components/search_container.js
+++ bench_bnb/frontend/components/search_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
 
 import { fetchBenches } from '../actions/bench_actions';
-import { changeFilter } from '../actions/filter_actions';
+import { changeFilter, updateFilter } from '../actions/filter_actions';
 import { selectAllBenches } from '../reducers/selector';
 import Search from './search';
 
 const mapStateToProps = (state) => ({
-  benches: selectAllBenches(state.benches)
+  benches: selectAllBenches(state.benches),
+  minSeating: state.filters.minSeating,
+  maxSeating: state.filters.maxSeating
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  changeFilter: (filter, value) => dispatch(changeFilter(filter, value))
+  changeFilter: (filter, value) => dispatch(changeFilter(filter, value)),
+  updateFilter: (filter, value) => dispatch(updateFilter(filter, value))
 });
 
 export default connect(
