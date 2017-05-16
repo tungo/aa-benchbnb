18699782982b0f7742bd72d008faca1b73feb16b
Tu Ngo <tungo8810@gmail.com>
2017-05-15 17:25:41 -0700

    Add updateFilter to reducer and actions

--- bench_bnb/frontend/actions/filter_actions.js
+++ bench_bnb/frontend/actions/filter_actions.js
@@ -1,11 +1,18 @@
 import { fetchBenches } from './bench_actions';
 
 export const UPDATE_BOUNDS = "UPDATE_BOUNDS";
+export const UPDATE_FILTER = "UPDATE_FILTER";
 
 
 export const updateBounds = (bounds) => ({
   type: UPDATE_BOUNDS,
   bounds
+});
+
+export const updateFilter = (filter, value) => ({
+  type: UPDATE_FILTER,
+  filter,
+  value
 });
 
 
--- bench_bnb/frontend/reducers/filters_reducer.js
+++ bench_bnb/frontend/reducers/filters_reducer.js
@@ -1,13 +1,23 @@
 import merge from 'lodash/merge';
-import { UPDATE_BOUNDS } from '../actions/filter_actions';
+import { UPDATE_BOUNDS, UPDATE_FILTER } from '../actions/filter_actions';
 
-const FiltersReducer = (state = {}, action) => {
+const _nullState = {
+  bounds: {},
+  minSeating: 1,
+  maxSeating: 10
+};
+
+const FiltersReducer = (state = _nullState, action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case UPDATE_BOUNDS:
       const nextState = merge({}, state, {bounds: action.bounds});
       return nextState;
+
+    case UPDATE_FILTER:
+      const { filter, value } = action;
+      return merge({}, state, {[filter]: value});
 
     default:
       return state;
